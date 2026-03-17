34777cd18667148595572248dbd2cfe409d9fd32
Nathan <na.alger05@gmail.com>
2026-03-17 13:07:25 -0400

    temp: hardcoded instructions

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include "Processor.h"
+#include <iostream>
 
 int main()
 {
    // Create a central processor with 1GB of RAM.
-   Processor centralProcessor(1024 * 1024 * 1024);
+   Processor cpu(1024 * 1024 * 1024);
+
+   cpu.memory.writeWord(0, 0x00500093); // addi x1, x0, 5
+   cpu.memory.writeWord(4, 0x00a00113); // addi x2, x0, 10
+   cpu.memory.writeWord(8, 0x002081b3); // add x3, x1, x2
+
+   cpu.program_counter = 0;
+
+   cpu.step();
+   cpu.step();
+   cpu.step();
+
+   std::cout << cpu.registers[3] << std::endl; // should be 15
 
    return 0;
 }
